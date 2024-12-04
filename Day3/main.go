6e52448e375615b1489ac921a83a012fb3da7d2a
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-12-03 17:27:01 -0700

    Implement part 2 of day 3

--- Day3/main.go
+++ Day3/main.go
@@ -8,6 +8,13 @@ import (
 	"strings"
 )
 
+func ParseMul(mulStr string) int {
+	operands := strings.Split(mulStr[4:len(mulStr)-1], ",")
+	lhs, _ := strconv.Atoi(operands[0])
+	rhs, _ := strconv.Atoi(operands[1])
+	return lhs * rhs
+}
+
 func main() {
 	// read the file
 	fileBytes, err := os.ReadFile("input.txt")
@@ -15,16 +22,31 @@ func main() {
 	if err != nil {
 		return
 	}
+	// part 1
 	re := regexp.MustCompile("mul\\([0-9]+,[0-9]+\\)")
 	commands := re.FindAllString(input, -1)
-	var lhs, rhs int
-	var operands []string
 	sum := 0
 	for i := 0; i < len(commands); i++ {
-		operands = strings.Split(commands[i][4:len(commands[i])-1], ",")
-		lhs, _ = strconv.Atoi(operands[0])
-		rhs, _ = strconv.Atoi(operands[1])
-		sum += lhs * rhs
+		sum += ParseMul(commands[i])
 	}
-	fmt.Printf("Sum: %v\n", sum)
+	fmt.Printf("Part one sum: %v\n", sum)
+	// part 2
+	re = regexp.MustCompile("(mul\\([0-9]+,[0-9]+\\))|do(n't)?\\(\\)")
+	commands = re.FindAllString(input, -1)
+	sum = 0
+	active := true
+	for i := 0; i < len(commands); i++ {
+		switch strings.Split(commands[i], "(")[0] {
+		case "mul":
+			if active {
+				sum += ParseMul(commands[i])
+			}
+		case "do":
+			active = true
+		case "don't":
+			active = false
+
+		}
+	}
+	fmt.Printf("Part two sum: %v\n", sum)
 }
